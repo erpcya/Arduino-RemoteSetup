b3d68d2f490b3f6aa7f39af595f990b5b4b61ef7
Yamel Senih (http://erpya.com/) <ysenih@erpya.com>
2019-01-02 03:03:59 -0430

    Add improvement for handle wifi command and test wifi

--- src/RemoteSetup.cpp
+++ src/RemoteSetup.cpp
@@ -14,19 +14,39 @@
  ************************************************************************************/
 #include "RemoteSetup.h"
 #include "CommandUtil.h"
+#include <EEPROM.h>
+
+/**
+ * Struct used for save initial setup
+ */
+struct _SetupDataStruct {
+  String deviceName;
+  //	Network setup
+  String wifiSSID;
+  String wifiPSK;
+} _dataSaved;
 
 /**
  * Implement library RemoteSetup.h
- */
-
-/**
- * Constructor, get prefix for file, chip selected for out and file format
  */
 RemoteSetup::RemoteSetup(uint8_t interface) : _commandHandler(10, 11) {
 	_interface = interface;
 	_isSetupDone = false;
 	_commandHandler.setDelay(250);
 	_commandHandler.begin(9600);
+	//	Get Struct from EEPROM
+	//	Validate if setup is done
+	if(EEPROM.read(0) == SETUP_DONE_KEY) {
+		_isSetupDone = true;
+		EEPROM.get(SETUP_DATA_ADDRESS, _dataSaved);
+	} else {
+		_dataSaved = {
+				"",
+				"",
+				""
+		};
+		_isSetupDone = false;
+	}
 }
 
 /**
@@ -44,26 +64,109 @@ boolean RemoteSetup::runRemoteSetup() {
 		return true;
 	}
 	//	Else
-	return _commandHandler.readPort();
+	if(_commandHandler.readPort()) {
+		_dataSaved.deviceName = getAttribute(DEVICE_NAME_KEY);
+		_dataSaved.wifiSSID = getAttribute(SSID_KEY);
+		_dataSaved.wifiPSK = getAttribute(PSK_KEY);
+		return true;
+	}
+	//
+	return false;
 }
 
 /**
  * Get attribute from key
  */
 String RemoteSetup::getAttribute(String key) {
-    return "";
+	if(_commandHandler.hasValue()) {
+		String token;
+		String evaluateCommand = _commandHandler.getValue();
+		int startTokenIndex = evaluateCommand.indexOf(STX_CHARACTER);
+		while (startTokenIndex != -1) {
+			evaluateCommand = evaluateCommand.substring(startTokenIndex + 1, evaluateCommand.length());    // from first STX_CHARACTER
+		    //
+		    int endTokenIndex = evaluateCommand.indexOf(ETX_CHARACTER);                        // next ETX_CHARACTER
+		    //	Get individual command
+		    token = evaluateCommand.substring(0, endTokenIndex);
+		    //
+		    if (token.indexOf(VALUE_SEPARATOR) > 0) {
+		    	String findKey = token.substring(0, token.indexOf(VALUE_SEPARATOR));
+		    	String value = token.substring(token.indexOf(VALUE_SEPARATOR) + 1, token.length());
+		    	//  Add to command
+		    	if(findKey == key) {
+		    		return value;
+		    	}
+		    }
+		    //
+		    evaluateCommand = evaluateCommand.substring(endTokenIndex + 1, evaluateCommand.length());    // from second STX_CHARACTER
+		    startTokenIndex = evaluateCommand.indexOf(STX_CHARACTER);
+		}
+	}
+    return NO_VALUE_FOR_KEY;
 }
+
+
 
 /**
  * Send a attribute
  */
-void RemoteSetup::sendAttribute(String key, String value) {
-    return "";
+boolean RemoteSetup::sendAttribute(char key[], char value[]) {
+	_commandHandler.initCommand(REMOTE_SETUP);
+	_commandHandler.sendValue(key, value);
+	_commandHandler.endCommand();
+    return false;
 }
 
 /**
  * Save Setup
  */
 boolean RemoteSetup::saveSetup() {
-	return false;
+	EEPROM.update(0, SETUP_DONE_KEY);
+	EEPROM.put(SETUP_DATA_ADDRESS, _dataSaved);
+	return true;
+}
+
+/**
+ * Test Wifi from remote
+ */
+boolean RemoteSetup::testWifi() {
+	String wifiValue = getAttribute(TEST_WIFI_KEY);
+	return wifiValue != NO_VALUE_FOR_KEY;
+}
+
+/**
+ * Send Wifi result of test
+ */
+void RemoteSetup::sendWifiResult(boolean result) {
+	sendAttribute(TEST_WIFI_KEY, (result? "Y": "N"));
+}
+
+/**
+ * Clear Setup
+ */
+void RemoteSetup::clearSetup() {
+	for (int index = 0 ; index < EEPROM.length() ; index++) {
+		EEPROM.write(index, SETUP_CLEAR_KEY);
+	}
+}
+
+/**
+ * Get Device Name
+ */
+String RemoteSetup::getDeviceName() {
+    return _dataSaved.deviceName;
+}
+
+/**
+ * Get Wifi SSID
+ */
+String RemoteSetup::getWifiSSID() {
+    return _dataSaved.wifiSSID;
+}
+
+/**
+ * Get Wifi PSK
+ */
+String RemoteSetup::getWifiPSK() {
+    return _dataSaved.wifiPSK;
 }
--- src/RemoteSetup.h
+++ src/RemoteSetup.h
@@ -22,6 +22,20 @@
  */
 static const uint8_t BLUETOOTH_INTERFACE =  1; // Setup from bluetooth
 static const uint8_t WIFI_INTERFACE =		2; // Setup from Wifi
+/**
+ * Setup Done
+ */
+static const uint8_t SETUP_DONE_KEY =		777; // Setup flag
+static const uint8_t SETUP_CLEAR_KEY =		0; // Setup flag
+static const uint8_t SETUP_DATA_ADDRESS =	sizeof(int); // Struct flag
+/**	Device Name	*/
+static const char DEVICE_NAME_KEY[] = "DeviceName";
+/**	Wifi keys	*/
+static const char SSID_KEY[] = "SSID";
+static const char PSK_KEY[] = "PSK";
+static const char TEST_WIFI_KEY[] = "TestWifi";
+
+static const char NO_VALUE_FOR_KEY = 0;
 
 class RemoteSetup {
     public:
@@ -29,11 +43,18 @@ class RemoteSetup {
         boolean isSetupDone();
         boolean runRemoteSetup();
         String getAttribute(String key);
-        boolean sendAttribute(String key, String value);
+        boolean sendAttribute(char key[], char value[]);
         boolean saveSetup();
+        void clearSetup();
+        String getDeviceName();
+        String getWifiSSID();
+        String getWifiPSK();
+        boolean testWifi();
+        void sendWifiResult(boolean result);
     private:
     	CommandUtil _commandHandler;
     	uint8_t _interface;
     	boolean _isSetupDone;
+    	struct _SetupDataStruct;
 };
 #endif
